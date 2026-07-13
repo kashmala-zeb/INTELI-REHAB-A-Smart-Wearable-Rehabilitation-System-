230d0fcc2d6c051159dda57fe71479afd202968a
Kashmala Zeb <malaxeb7@gmail.com>
2026-07-13 16:07:10 +0500

    Update patient entity and data models to align with recovery setup parameters

--- lib/data/models/patient_model.dart
+++ lib/data/models/patient_model.dart
@@ -1,10 +1,43 @@
 import 'package:inteli_rehab/domain/entities/patient.dart';
 
-class PatientModel extends PatientSetupModel {
+class PatientModel extends RecoverySetupModel {
   PatientModel({
-    required super.targetPainScale,
-    required super.targetWeeklySessions,
-    required super.romGoalDegrees,
-    required super.recoveryGoals,
+    super.affectedArm,
+    super.rehabArea,
+    super.recoveryStage,
+    super.therapistAssigned,
+    super.therapistCode,
+    super.exerciseFrequency,
+    super.selectedDays = const [],
+    super.reminderTime,
+    super.profilePhoto,
   });
+
+  factory PatientModel.fromJson(Map<String, dynamic> json) {
+    return PatientModel(
+      affectedArm: json['affectedArm'] as String?,
+      rehabArea: json['rehabArea'] as String?,
+      recoveryStage: json['recoveryStage'] as String?,
+      therapistAssigned: json['therapistAssigned'] as bool?,
+      therapistCode: json['therapistCode'] as String?,
+      exerciseFrequency: json['exerciseFrequency'] as String?,
+      selectedDays: (json['selectedDays'] as List<dynamic>?)?.map((e) => e.toString()).toList() ?? const [],
+      reminderTime: json['reminderTime'] as String?,
+      profilePhoto: json['profilePhoto'] as String?,
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'affectedArm': affectedArm,
+      'rehabArea': rehabArea,
+      'recoveryStage': recoveryStage,
+      'therapistAssigned': therapistAssigned,
+      'therapistCode': therapistCode,
+      'exerciseFrequency': exerciseFrequency,
+      'selectedDays': selectedDays,
+      'reminderTime': reminderTime,
+      'profilePhoto': profilePhoto,
+    };
+  }
 }
--- lib/domain/entities/patient.dart
+++ lib/domain/entities/patient.dart
@@ -5,7 +5,19 @@ class RecoverySetupModel {
   bool? therapistAssigned;      // true, false
   String? therapistCode;        // Code e.g. 'ABC-2041'
   String? exerciseFrequency;    // 'Every Day', '5 Days/Week', '3 Days/Week', 'Custom'
-  List<String> selectedDays = []; // ['Mon', 'Wed', 'Fri'] etc.
+  List<String> selectedDays;    // ['Mon', 'Wed', 'Fri'] etc.
   String? reminderTime;         // 'Morning (8:00 AM)', 'Afternoon (1:00 PM)', 'Evening (6:00 PM)', 'Custom'
   String? profilePhoto;         // Path to image asset
+
+  RecoverySetupModel({
+    this.affectedArm,
+    this.rehabArea,
+    this.recoveryStage,
+    this.therapistAssigned,
+    this.therapistCode,
+    this.exerciseFrequency,
+    this.selectedDays = const [],
+    this.reminderTime,
+    this.profilePhoto,
+  });
 }
